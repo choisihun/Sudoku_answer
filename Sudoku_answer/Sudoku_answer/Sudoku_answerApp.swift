a85a9d1bc258170f1867b5769548654039021725
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-01-01 21:22:53 +0900

    init: Initial commit

---
+++ Sudoku_answer/Sudoku_answer/Sudoku_answerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sudoku_answerApp.swift
+//  Sudoku_answer
+//
+//  Created by 최시훈 on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Sudoku_answerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
