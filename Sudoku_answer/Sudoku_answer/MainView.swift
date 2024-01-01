973525f84f7a920732bdf0206879e93ed8cb7525
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-01-01 21:28:27 +0900

    create: add MainView

---
+++ Sudoku_answer/Sudoku_answer/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Sudoku_answer
+//
+//  Created by 최시훈 on 1/1/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
